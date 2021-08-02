21309007eea7369af3d1184ddedbcfb1362f2abd
Julian Cordero <jcordero@gty.org>
2021-08-02 13:01:52 -0700

    Set up api call

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { create as apiCreate } from "apisauce";
 
 export default function App() {
+  const myURL = "https://api.github.com";
+  const client = apiCreate({ baseURL: myURL });
+  const getItem = () => client.get("/repos/twitter/bootstrap/commits", {});
+
+  const loadData = async () => {
+    const response = await getItem();
+    if (response.ok) {
+      console.log(response.data);
+    } else {
+      Alert.alert("Error retrieving data", "Please pull down to refresh", [
+        {
+          text: "OK",
+        },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>GitHub Commits for the Bootstrap repository by Twitter</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
